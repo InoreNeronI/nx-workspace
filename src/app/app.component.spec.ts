04476c353feebed0ce22703f3ddeed8690f555b6
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-07-15 14:06:31 +0530

    Refactor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'observable-promise-learning'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('observable-promise-learning');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('observable-promise-learning app is running!');
-  });
 });
