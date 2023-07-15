eb17bfa2b3cf4fddde96cf75182e49b361e97a19
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-07-15 15:31:09 +0530

    Promise learning

--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-promise',
   templateUrl: './promise.component.html',
   styleUrls: ['./promise.component.scss']
 })
-export class PromiseComponent {
+export class PromiseComponent implements OnInit {
+  public isOnline: boolean = false;
+  public status: string = "offline";
+  public myPromise: any;
+
+  ngOnInit(): void {
+    this.getStatus();
+    this.myPromise.then(
+      (response: any) => {
+        console.log(response);
+        this.checkLogic();
+      },
+      (error: any) => {
+        console.log(error);
+      },
+      (complete: any) => {
+        console.log(complete);
+      }
+    );
+  }
+
+  public getStatus() {
+    console.log("getStatus called");
+    this.myPromise = new Promise<boolean>((resolve, reject) => {
+      setTimeout(() => {
+        console.log("Recieved status value");
+        this.isOnline = true;
+        // resolve(this.isOnline);
+        reject("DB error!")
+      }, 3000);
+    });
+  }
+
+  public checkLogic() {
+    console.log("checkLogic called");
+    if (this.isOnline) {
+      this.status = "online";
+    } else {
+      this.status = "offline";
+    }
+  }
 
 }
