7cc68e0f664467c77b99d11c5cfd315c50078446
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-07-15 15:54:35 +0530

    Observable learning

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-observable',
   templateUrl: './observable.component.html',
   styleUrls: ['./observable.component.scss']
 })
-export class ObservableComponent {
+export class ObservableComponent implements OnInit {
+  public data: number[] = [];
+  public count: number = 0;
+  public myObservable: any;
 
+  ngOnInit(): void {
+    this.myObservable = new Observable<number[]>((observer) => {
+      setInterval(() => {
+        this.count++;
+        this.data.push(this.count);
+        if (this.count < 5) {
+          observer.next(this.data);
+        } else {
+          observer.error("DB error!")
+          // observer.complete();
+        }
+      }, 1000);
+    });
+  }
+
+  public getData(): void {
+    this.myObservable.subscribe(
+      (response: any) => {
+        console.log(response);
+      },
+      (error: any) => {
+        console.log(error);
+      },
+      () => {
+        console.log("Observable complete!");
+      }
+    );
+    // console.log(this.data);
+  }
 }
