dbfdf709b9ebd858265bc0d838752f8fe464f79a
mmozos <martinmozos@gmail.com>
2023-11-25 07:36:46 +0100

    Refactored code and purged some unused packages

--- apps/responsive-layout/src/main.component.ts
+++ apps/responsive-layout/src/main.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
 
-@Component({ selector: 'nx-main', standalone: true, template: '' })
+@Component({
+  selector: 'nx-main',
+  standalone: true,
+  templateUrl: '../assets/templates/responsive-layout.html',
+})
 export class MainComponent {}
